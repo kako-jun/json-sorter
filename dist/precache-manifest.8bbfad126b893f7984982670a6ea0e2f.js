d42f8822f1ee75feb8d1720cd6a653b3a97a8a80
kako-jun <3541096+kako-jun@users.noreply.github.com>
2021-04-07 14:41:31 +0900

    update.

---
+++ dist/precache-manifest.8bbfad126b893f7984982670a6ea0e2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6273589fe9524e3d2706",
+    "url": "/css/index.c3d71572.css"
+  },
+  {
+    "revision": "f1d93e07ac5908fde920c9aa446e8b62",
+    "url": "/img/taiiku_maehe_narae.f1d93e07.png"
+  },
+  {
+    "revision": "172770935b53348f92fb9d03e7e07b1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfbaf3ec849d84b2de77",
+    "url": "/js/chunk-vendors.ca54b212.js"
+  },
+  {
+    "revision": "6273589fe9524e3d2706",
+    "url": "/js/index.ce7631f6.js"
+  },
+  {
+    "revision": "2fe023e12ff9b0a40fcb3b08513237f7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
